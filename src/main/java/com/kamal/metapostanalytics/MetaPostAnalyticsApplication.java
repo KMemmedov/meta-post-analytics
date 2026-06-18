27104e6dfdab2188d5d989fe2c2042ac6bcf7d20
KMemmedov <memmedovkamal005@gmail.com>
2026-06-18 22:52:44 +0400

    Initial Spring Boot project setup

---
+++ src/main/java/com/kamal/metapostanalytics/MetaPostAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.kamal.metapostanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaPostAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaPostAnalyticsApplication.class, args);
+	}
+
+}
